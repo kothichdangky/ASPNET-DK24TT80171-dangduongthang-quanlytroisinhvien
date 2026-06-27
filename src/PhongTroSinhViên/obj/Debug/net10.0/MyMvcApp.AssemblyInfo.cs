d7d1bfae0bb2e62ec94a03ce84c14a4f10f229d3
Duong Thang <thangsos8900@gmail.com>
2026-06-27 17:36:12 +0700

    13

--- src/PhongTroSinhViên/obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ src/PhongTroSinhViên/obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca54745a54bf5752bacdf3963992d880f54bc6e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44fb4ff946e3965e0fbc611d5fc3478ad83746b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
